d3356a3cb2295596beb6fc95b1a5e2d679d7e709
Yunzhe <yunzhe@zju.edu.cn>
2020-05-23 22:01:51 +0800

    Fix GraphiQL assets in prod.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,1 @@
-if Rails.env.development?
-  Rails.application.config.assets.precompile += %w[graphiql/rails/application.js graphiql/rails/application.css]
-end
+Rails.application.config.assets.precompile += %w[graphiql/rails/application.js graphiql/rails/application.css]
