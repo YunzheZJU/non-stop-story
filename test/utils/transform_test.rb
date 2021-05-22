5f8277296267378c98d25008d6990be6a1842173
Yunzhe <yunzhe@zju.edu.cn>
2021-05-22 10:26:45 +0800

    Add tests.

--- test/utils/transform_test.rb
+++ test/utils/transform_test.rb
@@ -6,11 +6,16 @@ require 'utils/transform'
 class TransformTest < ActionView::TestCase
   test 'allocate' do
     w1 = %w[w1]
+    w1dup = %w[w1 w1]
     w3 = %w[w1 w2 w3]
     c1 = %w[c1]
     c6 = %w[c1 c2 c3 c4 c5 c6]
     c7 = %w[c1 c2 c3 c4 c5 c6 c7]
     assert_equal({ ['w1', 0] => %w[c1] }, Transform.allocate(w1, c1))
+    assert_equal(
+      { ['w1', 0] => %w[c1 c3 c5], ['w1', 1] => %w[c2 c4 c6] },
+      Transform.allocate(w1dup, c6)
+    )
     assert_equal({ ['w1', 0] => %w[c1] }, Transform.allocate(w3, c1))
     assert_equal(
       { ['w1', 0] => %w[c1 c4], ['w2', 1] => %w[c2 c5], ['w3', 2] => %w[c3 c6] },
